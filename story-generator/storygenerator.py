915e7cc4e835059f9ade17fe78c116c0ad994c23
darun000 <reachoutdarun@gmail.com>
2026-07-18 19:32:04 +0530

    Add story generator project

---
+++ story-generator/storygenerator.py
@@ -0,0 +1,31 @@
+import random
+
+when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago', 'On 20th Jan']
+
+who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle', 'a cat']
+
+name = ['Ali', 'David', 'Daniel', 'Harish', 'Sanjay']
+
+residence = ['Barcelona', 'India', 'Germany', 'Venice', 'England']
+
+went = ['cinema', 'university', 'seminar', 'school', 'laundry']
+
+happened = [
+    'made a lot of friends',
+    'ate a burger',
+    'found a secret key',
+    'solved a mystery',
+    'wrote a book'
+]
+
+print(
+    random.choice(when) + ", " +
+    random.choice(who) + " named " +
+    random.choice(name) +
+    " that lived in " +
+    random.choice(residence) +
+    ", went to the " +
+    random.choice(went) +
+    " and " +
+    random.choice(happened) + "."
+)
